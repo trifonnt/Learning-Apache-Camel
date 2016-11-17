f8c2854d3c003a579f66dc64f9db183b8f0bb0d9
Trifon Trifonov <trifont@gmail.com>
2016-11-17 19:50:16 +0200

    Added Eclipse project files.

--- src/main/java/example/ExampleProcessor.java
+++ src/main/java/example/ExampleProcessor.java
@@ -5,12 +5,12 @@ import org.apache.camel.Processor;
 
 public class ExampleProcessor implements Processor {
 
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        String name = exchange.getIn().getBody(String.class);
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		String name = exchange.getIn().getBody(String.class);
 
-        String response = "Hello, " + name + "!";
+		String response = "Hello, " + name + "!";
 
-        exchange.getOut().setBody(response);
-    }
+		exchange.getOut().setBody(response);
+	}
 }
--- src/main/java/example/ExampleServlet.java
+++ src/main/java/example/ExampleServlet.java
@@ -1,11 +1,6 @@
 package example;
 
-import org.apache.camel.ExchangePattern;
-import org.apache.camel.ProducerTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import java.io.IOException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,42 +9,45 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.apache.camel.ProducerTemplate;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class ExampleServlet extends HttpServlet {
 
-    private static final String DEFAULT_NAME = "World";
+	private static final String DEFAULT_NAME = "World";
 
-    private ProducerTemplate producer;
+	private ProducerTemplate producer;
 
-    @Override
-    public void init() throws ServletException {
-        producer = getSpringContext().getBean("exampleProducerTemplate", ProducerTemplate.class);
-    }
+	@Override
+	public void init() throws ServletException {
+		producer = getSpringContext().getBean("exampleProducerTemplate", ProducerTemplate.class);
+	}
 
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 
-        String endpoint = "direct:inbox";
-        String input = getName(request);
-        String output = producer.requestBody(endpoint, input, String.class);
+		String endpoint = "direct:inbox";
+		String input = getName(request);
+		String output = producer.requestBody(endpoint, input, String.class);
 
-        response.getOutputStream().println(output);
-    }
+		response.getOutputStream().println(output);
+	}
 
-    private String getName(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        if(name != null) {
-            return name;
-        }
-        return DEFAULT_NAME;
-    }
+	private String getName(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		if (name != null) {
+			return name;
+		}
+		return DEFAULT_NAME;
+	}
 
-    private WebApplicationContext getSpringContext() {
-        ServletContext servletContext = getServletContext();
-        WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-        return context;
-    }
+	private WebApplicationContext getSpringContext() {
+		ServletContext servletContext = getServletContext();
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+		return context;
+	}
 }
